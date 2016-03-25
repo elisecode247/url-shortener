d0012fa45a593115d63ef8867bfcb96d91494d7f
Elise <elisecode247@gmail.com>
2016-03-25 20:44:49 +0000

    production ready by changing mongo connection string

--- config/index.js
+++ config/index.js
@@ -1,7 +1,5 @@
-var configValues = require('../.git/.gitignore/config')
-
 module.exports = { 
     getDbConnectionString: function(){ 
-        return 'mongodb://' + configValues.uname + ":" + configValues.pwd + '@ds025429.mlab.com:25429/shortenedurls'
+        return process.env.MONGOHQ_URL
     }
 }
