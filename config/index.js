75bc5c20c813f0bc1d47a39e7cf5de73929c651c
Elise <elisecode247@gmail.com>
2016-03-27 15:22:02 +0000

    removed mongodb password

--- config/index.js
+++ config/index.js
@@ -2,4 +2,8 @@ module.exports = {
     getDbConnectionString: function(){ 
         return process.env.MONGOHQ_URL
     }
+}module.exports = { 
+    getDbConnectionString: function(){ 
+        return process.env.MONGOHQ_URL
+    }
 }
